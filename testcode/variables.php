7debc9089581e8a5560bab2fda6defdde285d1ab
Casey Hyde <hyde.casey@gmail.com>
2017-03-03 18:06:27 -0600

    Add multiline comment

--- testcode/variables.php
+++ testcode/variables.php
@@ -1,7 +1,9 @@
 <?php
 $firstname = 'Casey';
 $number = 4200.99;
-
+/****************************
+This is a multiline comment
+*****************************/
 echo $number, ' ', $firstname;
 print $firstname;
 //This is a comment..
